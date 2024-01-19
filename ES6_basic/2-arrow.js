071d1d4dd2d17688e8a50aff09dc2217c6994af0
santacoll19 <collazokel19@gmail.com>
2024-01-18 22:01:43 -0400

    yes?

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default function getNeighborhoodsList(){
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-  self.sanFranciscoNeighborhoods.push(newNeighborhood);
-  return self.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
