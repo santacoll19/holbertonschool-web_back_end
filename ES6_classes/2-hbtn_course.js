8e4ab2f9b748463e8ea4e91f1499f9133d7445fc
santacoll19 <collazokel19@gmail.com>
2024-01-19 14:15:04 -0400

    i guess

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,14 +2,14 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw TypeError('Name must be a string');
     if (typeof length !== 'number') throw TypeError('Length must be a number');
-    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
     throw TypeError('Students must be an array of strings');
     }
 
     this._name = name;
     this._length = length;
     this._students = students;
-    }
+  }
 
   get name() {
     return this._name;
@@ -31,12 +31,12 @@ export default class HolbertonCourse {
 
   get students() {
     return this._students;
-    }
+  }
 
   set students(newStudents) {
     if (!Array.isArray(newStudents) || newStudents.some((student) => typeof student !== 'string')) {
-        throw TypeError('Students must be an array of strings');
-      }
-      this._students = newStudents;
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
     }
 }
