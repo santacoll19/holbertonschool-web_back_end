6698536e8aed5dd0a3da5bf74f221afd6fd4b590
santacoll19 <collazokel19@gmail.com>
2024-01-18 22:36:20 -0400

    ugh

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,17 +1,17 @@
 function getCurrentYear()
 {
-    const date = new Date();
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita)
 {
-    const currentYear = getCurrentYear();
-    const budget = {
+  const currentYear = getCurrentYear();
+  const budget = {
 
-    [`income-${currentYear}`]: income,
-    [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita,
-    };
-    return budget;
+  [`income-${currentYear}`]: income,
+  [`gdp-${currentYear}`]: gdp,
+  [`capita-${currentYear}`]: capita,
+  };
+  return budget;
 }
