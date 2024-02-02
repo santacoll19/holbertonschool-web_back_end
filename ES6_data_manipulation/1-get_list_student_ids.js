748bb39bd17d8279f418088c667f98576519c0ec
santacoll19 <collazokel19@gmail.com>
2024-02-02 11:50:48 -0400

    test

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+    return [];
+    }
+    const ids = students.map((student) => student.id);
+    return ids;
+}
+
+export default getListStudentIds;
