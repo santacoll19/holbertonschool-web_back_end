b0b4a6734d4c41b9e4350814f9b409c1d00cf641
santacoll19 <collazokel19@gmail.com>
2024-02-02 12:24:53 -0400

    yes!

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,12 +2,12 @@ function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0);
 
-    if (position > length - 1) {
+  if (position > length - 1) {
     throw Error('Position outside range');
-    }
+  }
 
-    view.setInt8(position, value);
-    return view;
+  view.setInt8(position, value);
+  return view;
 }
 
 export default createInt8TypedArray;
